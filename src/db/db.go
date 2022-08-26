f1ba9ede25575eedac088b31b2236163b6faa42c
Toma <13.35.104@gmail.com>
2022-08-26 09:38:29 +0900

    gormをインストール

---
+++ src/db/db.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	// id         int16
+	first_name string
+	last_name  string
+	email      string
+}
+
+func main() {
+	dsn := "user:password@tcp(gin-jwt-react-query-auth_db_1:3306)/database?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
+	// Create
+	db.Create(&User{first_name: "toma", last_name: "morisawa", email: "morisawa@exmaple.com"})
+
+	// Read
+	var product User
+	db.First(&product, 1)                           // find product with integer primary key
+	db.First(&product, "last_name = ?", "morisawa") // find product with code D42
+
+	// Update - update product's price to 200
+	db.Model(&product).Update("first_name", "toma")
+	// Update - update multiple fields
+	db.Model(&product).Updates(User{first_name: "toma2", last_name: "morisawa2", email: "morisawa@exmaple.com"})
+	db.Model(&product).Updates(map[string]interface{}{"first_name": "toma3", "last_name": "morisawa3"})
+
+	// Delete - delete product
+	db.Delete(&product, 1)
+}
--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,8 @@ module github.com/hrs-o/docker-go
 go 1.16
 
 require (
-	github.com/appleboy/gin-jwt/v2 v2.8.0 // indirect
+	github.com/appleboy/gin-jwt/v2 v2.8.0
 	github.com/gin-gonic/gin v1.7.7
+	gorm.io/driver/mysql v1.3.6
+	gorm.io/gorm v1.23.8
 )
