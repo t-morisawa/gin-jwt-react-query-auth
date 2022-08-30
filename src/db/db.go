1ef5411f9feefd7aaa9fb0c5c46b31988fffc005
Toma <13.35.104@gmail.com>
2022-08-30 17:28:55 +0900

    ログイン用の関数を追加

--- src/db/db.go
+++ src/db/db.go
@@ -10,9 +10,9 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"size:255"`
-	Password string `gorm:"size:255"`
 	Email    string `gorm:"size:255"`
+	Password string `gorm:"size:255"`
+	Username string `gorm:"size:255"`
 }
 
 const DSN = "user:password@tcp(gin-jwt-react-query-auth_db_1:3306)/database?charset=utf8mb4&parseTime=True&loc=Local"
@@ -22,8 +22,8 @@ func passwordEncrypt(password string) (string, error) {
 	return string(hash), err
 }
 
-func compareHashAndPassword(hash, password string) error {
-	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+func compareHashAndPassword(hash, passwordRaw string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(passwordRaw))
 }
 
 func CreateUser(user *User) error {
@@ -35,4 +35,27 @@ func CreateUser(user *User) error {
 	db.Create(user)
 
 	return nil
+}
+
+func GetUser(email string) (*User, error) {
+	db, err := gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	if err != nil {
+		return nil, errors.New("failed to connect database")
+	}
+	var user User
+	db.First(&user, "email = ?", email)
+
+	return &user, nil
+}
+
+func Login(email string, passwordRaw string) (*User, error) {
+	user, err := GetUser(email)
+	if err != nil {
+		return nil, errors.New("failed to login")
+	}
+	err = compareHashAndPassword(user.Password, passwordRaw)
+	if err != nil {
+		return nil, errors.New("wrong password")
+	}
+	return user, nil
 }
